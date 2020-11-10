d23fe51dd13f746630532e5c081a4851031ef508
EC2 Default User <ec2-user@ip-172-31-81-204.ec2.internal>
2020-11-10 09:06:18 +0000

    Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,19 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
     @books = Book.all
-    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    @books = Book.all
+    if @book.save
+      redirect_to book_path(@book)
+      flash[:notice] = "You have created book successfully."
+    else
+      render :index
+    end
   end
 
   def show
@@ -21,9 +26,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+      flash[:notice] = "You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
 
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 200 }
+
 end
